d877b6d0077bb2986e15aa2d3e5f17bc73994a9d
sabindangi <sabin4.dangi@gmail.com>
2026-02-01 00:57:36 -0600

    adding message to the book-online services

--- src/app/book-online/book-online.component.ts
+++ src/app/book-online/book-online.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-online',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-online.component.html',
   styleUrl: './book-online.component.css'
 })
 export class BookOnlineComponent {
+  unavailableMessage = '';
+  private hideTimeoutId: number | null = null;
 
+  showUnavailable(serviceName: string): void {
+    this.unavailableMessage = `Sorry, ${serviceName} is not yet available for online bookings. Please contact us for more information.`;
+    if (this.hideTimeoutId !== null) {
+      window.clearTimeout(this.hideTimeoutId);
+    }
+    this.hideTimeoutId = window.setTimeout(() => {
+      this.unavailableMessage = '';
+      this.hideTimeoutId = null;
+    }, 4000);
+  }
 }
